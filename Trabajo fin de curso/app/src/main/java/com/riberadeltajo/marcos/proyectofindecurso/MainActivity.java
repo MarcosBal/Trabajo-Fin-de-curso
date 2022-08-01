c370f161fe940bea7d3814eda78e46d051a1338d
MarcosBal <78751584+MarcosBal@users.noreply.github.com>
2022-08-01 17:33:58 +0200

    Add files via upload

---
+++ Trabajo fin de curso/app/src/main/java/com/riberadeltajo/marcos/proyectofindecurso/MainActivity.java
@@ -0,0 +1,14 @@
+package com.riberadeltajo.marcos.proyectofindecurso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
