5295787ece7c204d05e2f52f1c1881c2e181d377
M.Ballesteros <78751584+MarcosBal@users.noreply.github.com>
2022-08-02 23:56:17 +0200

    Add: login and Splash Screen

--- Trabajo fin de curso/app/src/main/java/com/riberadeltajo/marcos/proyectofindecurso/MainActivity.java
+++ Trabajo fin de curso/app/src/main/java/com/riberadeltajo/marcos/proyectofindecurso/MainActivity.java
@@ -2,13 +2,32 @@ package com.riberadeltajo.marcos.proyectofindecurso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvNotLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.ProyectoFinDeCurso);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvNotLogin = findViewById(R.id.tvNotLogin);
+        tvNotLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), InicioActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
