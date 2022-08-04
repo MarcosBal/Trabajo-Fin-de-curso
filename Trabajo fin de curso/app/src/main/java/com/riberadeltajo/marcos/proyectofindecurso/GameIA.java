8e7ae0520a98b475fd147a27f266324625ce0b42
Marcos Ballesteros <m.ballesteros@meytel.net>
2022-08-04 20:56:45 +0200

    Add: Vistas y empezar con lógica del juego

---
+++ Trabajo fin de curso/app/src/main/java/com/riberadeltajo/marcos/proyectofindecurso/GameIA.java
@@ -0,0 +1,14 @@
+package com.riberadeltajo.marcos.proyectofindecurso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameIA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_ia);
+    }
+}
--- Trabajo fin de curso/app/src/main/java/com/riberadeltajo/marcos/proyectofindecurso/InicioActivity.java
+++ Trabajo fin de curso/app/src/main/java/com/riberadeltajo/marcos/proyectofindecurso/InicioActivity.java
@@ -1,14 +1,38 @@
 package com.riberadeltajo.marcos.proyectofindecurso;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class InicioActivity extends AppCompatActivity {
+public class InicioActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ConstraintLayout constraintPvIA;
+    ConstraintLayout constraintPvP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        constraintPvIA = findViewById(R.id.constraintPvIA);
+        constraintPvP = findViewById(R.id.constraintPvP);
+
+        constraintPvP.setOnClickListener(this);
+        constraintPvIA.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = null;
+        if (v.getId() == constraintPvIA.getId()){
+           i = new Intent(this, GameIA.class);
+        }
+        startActivity(i);
+
     }
 }
