a7aa1363ec4fa087628a1f15905c8c9259635a4c
Marcos Ballesteros <m.ballesteros@meytel.net>
2022-09-01 22:17:14 +0200

    Add: firebase login and game with AI

--- Trabajo fin de curso/app/src/main/java/com/riberadeltajo/marcos/proyectofindecurso/GameIA.java
+++ Trabajo fin de curso/app/src/main/java/com/riberadeltajo/marcos/proyectofindecurso/GameIA.java
@@ -1,35 +1,208 @@
 package com.riberadeltajo.marcos.proyectofindecurso;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class GameIA extends AppCompatActivity {
+import java.util.Arrays;
+
+public class GameIA extends AppCompatActivity implements View.OnClickListener {
 
     Tablero t;
     int numeroFilasYColumnas = 3;
+    int movi = 0;
+    boolean win = false;
+    ImageView t1,t2,t3,t4,t5,t6,t7,t8,t9;
+    Button btnReplay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_ia);
+        t1 = findViewById(R.id.t1);
+        t2 = findViewById(R.id.t2);
+        t3 = findViewById(R.id.t3);
+        t4 = findViewById(R.id.t4);
+        t5 = findViewById(R.id.t5);
+        t6 = findViewById(R.id.t6);
+        t7 = findViewById(R.id.t7);
+        t8 = findViewById(R.id.t8);
+        t9 = findViewById(R.id.t9);
+        btnReplay = findViewById(R.id.replay);
+        btnReplay.setOnClickListener(this);
+        t1.setOnClickListener(this);
+        t2.setOnClickListener(this);
+        t3.setOnClickListener(this);
+        t4.setOnClickListener(this);
+        t5.setOnClickListener(this);
+        t6.setOnClickListener(this);
+        t7.setOnClickListener(this);
+        t8.setOnClickListener(this);
+        t9.setOnClickListener(this);
         empezarJuego(numeroFilasYColumnas);
+
     }
 
     private void empezarJuego(int numeroFilasYColumnas){
         t = new Tablero(numeroFilasYColumnas);
-        comprobarTamañoPantalla(t.getFilasColumnas());
+        t.setJugador(Tablero.estado.X);
+        if (t.turno().equals("IA")){
+            movimientoIA();
+        }
     }
 
-    private void comprobarTamañoPantalla(int filasYColunas){
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x;
-        int height = size.y;
 
-        
+    @Override
+    public void onClick(View v) {
+        if (!win) {
+            if (v.getId() == t1.getId()) {
+                if (t.comprobarPosi(0,2)) {
+                    v.setBackgroundColor(Color.RED);
+                    movimientoJugador(0, 2);
+                }
+            }
+            if (v.getId() == t2.getId()) {
+                if (t.comprobarPosi(1,2)) {
+                    v.setBackgroundColor(Color.RED);
+                    movimientoJugador(1, 2);
+                }
+            }
+            if (v.getId() == t3.getId()) {
+                if (t.comprobarPosi(2,2)) {
+                    v.setBackgroundColor(Color.RED);
+                    movimientoJugador(2, 2);
+                }
+            }
+            if (v.getId() == t4.getId()) {
+                if (t.comprobarPosi(0,1)) {
+                    v.setBackgroundColor(Color.RED);
+                    movimientoJugador(0, 1);
+                }
+            }
+            if (v.getId() == t5.getId()) {
+                if (t.comprobarPosi(1,1)) {
+                    v.setBackgroundColor(Color.RED);
+                    movimientoJugador(1, 1);
+                }
+            }
+            if (v.getId() == t6.getId()) {
+                if (t.comprobarPosi(2,1)) {
+                    v.setBackgroundColor(Color.RED);
+                    movimientoJugador(2, 1);
+                }
+            }
+            if (v.getId() == t7.getId()) {
+                if (t.comprobarPosi(0,0)) {
+                    v.setBackgroundColor(Color.RED);
+                    movimientoJugador(0, 0);
+                }
+            }
+            if (v.getId() == t8.getId()) {
+                if (t.comprobarPosi(1,0)) {
+                    v.setBackgroundColor(Color.RED);
+                    movimientoJugador(1, 0);
+                }
+            }
+            if (v.getId() == t9.getId()) {
+                if (t.comprobarPosi(2,0)) {
+                    v.setBackgroundColor(Color.RED);
+                    movimientoJugador(2, 0);
+                }
+            }
+        }
+        if (v.getId() == btnReplay.getId()){
+            reiniciar();
+        }
+    }
+
+    private void reiniciar(){
+        win = false;
+        movi = 0;
+        t1.setBackgroundColor(Color.WHITE);
+        t2.setBackgroundColor(Color.WHITE);
+        t3.setBackgroundColor(Color.WHITE);
+        t4.setBackgroundColor(Color.WHITE);
+        t5.setBackgroundColor(Color.WHITE);
+        t6.setBackgroundColor(Color.WHITE);
+        t7.setBackgroundColor(Color.WHITE);
+        t8.setBackgroundColor(Color.WHITE);
+        t9.setBackgroundColor(Color.WHITE);
+        t.reiniciar();
+        if (t.turno().equals("IA")){
+            movimientoIA();
+        }
+    }
+
+    private void movimientoJugador(int x, int y){
+        Posicion p;
+        movi++;
+        t.movimiento(x, y, t.getJugador(), t.getTablero());
+        if(!t.comprobarVictoria(t.getTablero(), t.getJugador(), x, y)){
+            if (!t.comprobarEmpate(movi)){
+                movimientoIA();
+            }else{
+                win = true;
+            }
+        }else{
+            win = true;
+        }
+    }
+
+    private void movimientoIA(){
+        movi++;
+        Posicion p;
+        p = t.movimientoIA(t.getTablero());
+        t.movimiento(p.getX(),p.getY(), t.getIA(),t.getTablero());
+        pintarIA(p);
+        if(!t.comprobarVictoria(t.getTablero(), t.getIA(), p.getX(),p.getY())){
+            if (t.comprobarEmpate(movi)){
+                win = true;
+            }
+        }else{
+            win = true;
+        }
+    }
+
+    private void pintarIA(Posicion p) {
+        if(p.getX() == 0 && p.getY() == 2){
+            t1.setBackgroundColor(Color.BLUE);
+        }
+        if(p.getX() == 1 && p.getY() == 2){
+            t2.setBackgroundColor(Color.BLUE);
+        }
+        if(p.getX() == 2 && p.getY() == 2){
+            t3.setBackgroundColor(Color.BLUE);
+        }
+        if(p.getX() == 0 && p.getY() == 1){
+            t4.setBackgroundColor(Color.BLUE);
+        }
+        if(p.getX() == 1 && p.getY() == 1){
+            t5.setBackgroundColor(Color.BLUE);
+        }
+        if(p.getX() == 2 && p.getY() == 1){
+            t6.setBackgroundColor(Color.BLUE);
+        }
+        if(p.getX() == 0 && p.getY() == 0){
+            t7.setBackgroundColor(Color.BLUE);
+        }
+        if(p.getX() == 1 && p.getY() == 0){
+            t8.setBackgroundColor(Color.BLUE);
+        }
+        if(p.getX() == 2 && p.getY() == 0){
+            t9.setBackgroundColor(Color.BLUE);
+        }
     }
 }
--- Trabajo fin de curso/app/src/main/java/com/riberadeltajo/marcos/proyectofindecurso/Tablero.java
+++ Trabajo fin de curso/app/src/main/java/com/riberadeltajo/marcos/proyectofindecurso/Tablero.java
@@ -1,11 +1,13 @@
 package com.riberadeltajo.marcos.proyectofindecurso;
 
+import java.util.Random;
+
 public class Tablero {
 
-    private enum estado {Vacio, X, O}
-    private estado tablero[][];
+    public enum estado {Vacio, X, O}
+    private estado[][] tablero;
+    private estado jugador, IA;
     private int filasColumnas;
-    private int numeroMovi;
 
     public Tablero(int filasColumnas) {
         this.filasColumnas = filasColumnas;
@@ -28,41 +30,130 @@ public class Tablero {
         this.filasColumnas = filasColumnas;
     }
 
-    public int getNumeroMovi() {
-        return numeroMovi;
+    public estado getJugador() {
+        return jugador;
     }
 
-    public void setNumeroMovi(int numeroMovi) {
-        this.numeroMovi = numeroMovi;
+    public void setJugador(estado jugador) {
+        this.jugador = jugador;
+    }
+
+    public estado getIA() {
+        return IA;
+    }
+
+    public void setIA(estado IA) {
+        this.IA = IA;
     }
 
     private void rellenarTablero(){
         tablero = new estado[filasColumnas][filasColumnas];
+        for (int i = 0; i <3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tablero[i][j] = estado.Vacio;
+            }
+        }
     }
 
-    private void Move(int x, int y, estado s){
-        if(tablero[x][y] == estado.Vacio){
-            tablero[x][y] = s;
+    public String turno(){
+        Random r = new Random();
+        if(r.nextInt(2) == 0)
+            return "IA";
+        else
+            return "J";
+    }
+
+    public void movimiento(int x, int y, estado s, estado[][] tab){
+        tab[x][y] = s;
+    }
+
+    public Posicion movimientoIA(estado[][] tabl){
+        Posicion posi = new Posicion();
+        if(jugador == estado.X){
+            IA = estado.O;
+        }else{
+            IA = estado.X;
         }
-        numeroMovi++;
+        estado[][] copiaTablero;
+        //Compruebo que la ia pueda ganar en el siguiente mov
+        for (int i = 0; i <3; i++) {
+            for (int j = 0; j < 3; j++) {
+                copiaTablero = getCopiaTablero(tabl);
+                if(copiaTablero[i][j] == estado.Vacio){
+                    movimiento(i, j, IA, copiaTablero);
+                    if(comprobarVictoria(copiaTablero, IA, i, j)){
+                        posi.setX(i);
+                        posi.setY(j);
+                        return posi;
+                    }
+                }
+            }
+        }
 
-        //Comprobar condicion de victoria
+        //Compruebo que el jugador pueda ganar en el siguente mov para bloquerlo
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                copiaTablero = getCopiaTablero(tabl);
+                if(copiaTablero[i][j] == estado.Vacio){
+                    movimiento(i, j, jugador, copiaTablero);
+                    if(comprobarVictoria(copiaTablero, jugador, i, j)){
+                        posi.setX(i);
+                        posi.setY(j);
+                        return posi;
+                    }
+                }
+            }
+        }
 
+        //Intenta coger las esquinas si estan libres
+        posi = escogerEsquina(tabl);
+        if (posi != null){
+            //movimiento(posi.getX(), posi.getY(), IA, tabl);
+            return posi;
+        }
+
+        //Intenta coger el centro si está libre
+        if (tabl[1][1] == estado.Vacio) {
+            //movimiento(1, 1, IA, tabl);
+            posi.setX(1);
+            posi.setY(1);
+            return posi;
+        }
+
+        //Intenta coger los lados si entan libres
+        posi = escogerLado(tabl);
+        //movimiento(posi.getX(), posi.getY(), IA, tabl);
+        return posi;
+    }
+
+    private estado[][] getCopiaTablero (estado[][] tab){
+        estado[][] copia = new estado[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                copia[i][j] = tab[i][j];
+            }
+        }
+        return copia;
+    }
+
+    public boolean comprobarVictoria(estado[][] tab, estado s, int x, int y) {
         //Comprobar columnas
         for(int i = 0; i < filasColumnas; i++){
-            if(tablero[x][i] != s)
+            if(tab[x][i] != s)
                 break;
             if(i == filasColumnas-1){
                 //report win for s
+                return true;
             }
         }
 
         //Comprobar filas
         for(int i = 0; i < filasColumnas; i++){
-            if(tablero[i][y] != s)
+            if(tab[i][y] != s)
                 break;
             if(i == filasColumnas-1){
                 //report win for s
+                return true;
             }
         }
 
@@ -70,10 +161,11 @@ public class Tablero {
         if(x == y){
             //we're on a diagonal
             for(int i = 0; i < filasColumnas; i++){
-                if(tablero[i][i] != s)
+                if(tab[i][i] != s)
                     break;
                 if(i == filasColumnas-1){
                     //report win for s
+                    return true;
                 }
             }
         }
@@ -81,21 +173,88 @@ public class Tablero {
         //Comprobar la otra diagonal
         if(x + y == filasColumnas - 1){
             for(int i = 0; i < filasColumnas; i++){
-                if(tablero[i][(filasColumnas-1)-i] != s)
+                if(tab[i][(filasColumnas-1)-i] != s)
                     break;
                 if(i == filasColumnas-1){
                     //report win for s
+                    return true;
                 }
             }
         }
+        return false;
+    }
 
-        //Comprobar empate
-        if(numeroMovi == (Math.pow(filasColumnas, 2) - 1)){
-            //report draw
+    private Posicion escogerLado(estado[][] tab) {
+        Posicion posi = new Posicion();
+        if (tab[0][1] == estado.Vacio){
+            posi.setX(0);
+            posi.setY(1);
+            return posi;
+        }
+
+        if (tab[1][0] == estado.Vacio){
+            posi.setX(1);
+            posi.setY(0);
+            return posi;
+        }
+
+        if (tab[2][1] == estado.Vacio){
+            posi.setX(2);
+            posi.setY(1);
+            return posi;
+        }
+
+        if (tab[1][2] == estado.Vacio){
+            posi.setX(1);
+            posi.setY(2);
+            return posi;
+        }
+
+        return null;
+    }
+
+    private Posicion escogerEsquina(estado[][] tab) {
+        Posicion posi = new Posicion();
+        if (tab[0][0] == estado.Vacio){
+            posi.setX(0);
+            posi.setY(0);
+            return posi;
+        }
+
+        if (tab[0][2] == estado.Vacio){
+            posi.setX(0);
+            posi.setY(2);
+            return posi;
+        }
+
+        if (tab[2][0] == estado.Vacio){
+            posi.setX(2);
+            posi.setY(0);
+            return posi;
+        }
+
+        if (tab[2][2] == estado.Vacio){
+            posi.setX(2);
+            posi.setY(2);
+            return posi;
+        }
+
+        return null;
+    }
+
+    public boolean comprobarEmpate(int numeroMov){
+        return numeroMov == (Math.pow(filasColumnas, 2) - 1);
+    }
+
+    public void reiniciar(){
+        for (int i = 0; i <3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tablero[i][j] = estado.Vacio;
+            }
         }
     }
 
-    private void reiniciar(){
-        rellenarTablero();
+    public boolean comprobarPosi(int x, int y){
+        return tablero[x][y] == estado.Vacio;
     }
 }
